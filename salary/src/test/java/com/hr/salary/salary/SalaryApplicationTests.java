635576dc31e215d8ee6413a3ca947357034776a7
tnie <tnie@deloitte.com.cn>
2020-10-19 18:23:09 +0800

    初始化项目结构

---
+++ salary/src/test/java/com/hr/salary/salary/SalaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.salary.salary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
